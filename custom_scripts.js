9a2eaabf8ffca576dbfa7baafe511eb4ec5331a9
Cammie <152633573+orchidevt@users.noreply.github.com>
2024-12-08 15:58:20 +0100

    Update custom_scripts.js
    Everything is in one file

--- custom_scripts.js
+++ custom_scripts.js
@@ -1,3 +1,31 @@
+function scrollie() {
+   window.scrollTo(0, 0);
+}
+
+function setDarkMode() {
+   let theeme = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
+   document.documentElement.setAttribute('data-bs-theme', theeme);
+}
+
+function toggleHeadingArrow(event) {
+   var triggerHeading = event.currentTarget;
+   var mid = triggerHeading.children[0];
+   var icn = mid.children[0];
+   const listy = icn.classList;
+   var fst, snd;
+   fst = snd = 'fa-angle-'
+   if (listy.contains('fa-angle-down')) {
+      fst += 'down';
+      snd += 'up';
+   }
+   else {
+      fst += 'up';
+      snd += 'down';
+   }
+   listy.remove(fst);
+   listy.add(snd);
+}
+
 function icons() {
    const titularHeading = document.getElementById("hd");
    titularHeading.innerHTML += '<i class="fa fa-paragraph"></i><a href="#" class="disabled"><i class="fa fa-language"></i></a>';
